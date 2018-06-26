82711f308e5d7f975eebe2889498a6507db5af23
Snp277 <38774700+Snp277@users.noreply.github.com>
2018-06-25 23:27:37 -0500

    ok

--- assignments/lesson-9/scripts/weather.js
+++ assignments/lesson-9/scripts/weather.js
@@ -1,91 +1,35 @@
-function addCommas(myValue) {
-    var Formated = myValue.toString().split(".");
-    Formated[0] = Formated[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-    return Formated.join(".");
-}
+let towninfoRequest = new XMLHttpRequest();
+let towninfoURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
 
+towninfoRequest.open('GET', towninfoURL, true);
+towninfoRequest.send();
 
-var town_data_Request = new XMLHttpRequest();
-    var data_URL = "https://byui-cit230.github.io/weather/data/towndata.json";
-    town_data_Request.open('GET', data_URL, true);
-    town_data_Request.send();
-    town_data_Request.onload =  function () {
-        let town_data = JSON.parse(town_data_Request.responseText);
-        var x;
-        var CurrentPage = document.getElementsByClassName("active")[0].getElementsByTagName("a")[0].innerHTML;
+towninfoRequest.onload = function() {
+  let townData = JSON.parse(towninfoRequest.responseText);
 
-        if (CurrentPage.toLowerCase() == "home") {
-        var cities = ["franklin", "greenville", "springfield"]
-        var city;
-        for (var y = 0; y < 3; y++){
-            city = cities[y];
-            x = 0;
-            while (town_data.towns[x].name.toLowerCase() != city) {
-                x++;
-            }
+  console.log(townData);
 
-            document.getElementById("motto-" + city).innerHTML = town_data.towns[x].motto;
-            document.getElementById("year-founded-" + city).innerHTML = town_data.towns[x].yearFounded;
-            document.getElementById("num-people-" + city).innerHTML = addCommas(town_data.towns[x].currentPopulation);
-            document.getElementById("rainy-days-" + city).innerHTML = town_data.towns[x].averageRainfall;
+  document.getElementById('Ftown-motto').innerHTML =  townData.towns[0].motto;
 
-        }
+  document.getElementById('Ftown-founded').innerHTML =  townData.towns[0].yearFounded;
 
-        } else {
-            var city = CurrentPage.toLowerCase();
-            x = 0;
+  document.getElementById('Ftown-population').innerHTML =  townData.towns[0].currentPopulation;
 
-            while (town_data.towns[x].name.toLowerCase() != city) {
-                x++;
-            }
+  document.getElementById('Ftown-rainfall').innerHTML =  townData.towns[0].averageRainfall;
+  
+  document.getElementById('Gtown-motto').innerHTML =  townData.towns[1].motto;
 
-            document.getElementById("name-data").innerHTML = town_data.towns[x].name;
-            document.getElementById("motto").innerHTML = town_data.towns[x].motto;
-            document.getElementById("year-founded").innerHTML = town_data.towns[x].yearFounded;
-            document.getElementById("num-people").innerHTML = addCommas(town_data.towns[x].currentPopulation);
-            document.getElementById("rainy-days").innerHTML = town_data.towns[x].averageRainfall;
+  document.getElementById('Gtown-founded').innerHTML =  townData.towns[1].yearFounded;
 
-            var z = 0;
-            var m = town_data.towns[x].events.length - 1;
-            var eventlist;
+  document.getElementById('Gtown-population').innerHTML =  townData.towns[1].currentPopulation;
 
-            for (var z = 0; z < m; z++) {
-                eventlist = document.createElement('li');
-                eventlist.textContent = town_data.towns[x].events[z];
-                document.getElementById("town-events").appendChild(eventlist);
-            }
+  document.getElementById('Gtown-rainfall').innerHTML =  townData.towns[1].averageRainfall;
 
-        }
-    }
+  document.getElementById('Stown-motto').innerHTML =  townData.towns[3].motto;
 
-   
+  document.getElementById('Stown-founded').innerHTML =  townData.towns[3].yearFounded;
 
-function franklinWeather(){
-    var weatherRequest = new XMLHttpRequest();
-    var apiURL = "https://api.openweathermap.org/data/2.5/weather?id=4156210&APPID=76a3770e7164ef89df9f9ede671deeeb&units=imperial";
-    weatherRequest.open('GET', apiURL, true);
-    weatherRequest.send();
-    weatherRequest.onload =  function () {
-        let weatherData = JSON.parse(weatherRequest.responseText);
-        console.log(weatherData);
-        var icon = weatherData.weather[0].icon;
-        let iconSrc = "https://openweathermap.org/img/w/"+icon+".png";
-        document.getElementById("current_temp").innerHTML = weatherData.main.temp;
-        document.getElementById("icon").setAttribute("src", iconSrc);
-        document.getElementById("sky").innerHTML = weatherData.weather[0].description;
-        document.getElementById("wind").innerHTML = weatherData.wind.speed;
-        }
-}
+  document.getElementById('Stown-population').innerHTML =  townData.towns[3].currentPopulation;
 
-function windchill() {
-    var high = parseFloat(document.getElementById("high").innerHTML);
-    var low = parseFloat(document.getElementById("low").innerHTML);
-    var s = parseFloat(document.getElementById("wind").innerHTML);
-    var windchill = Math.round(computewindchill(high, low, s));
-
-}
-function computewindchill(high, low, s){
-    var t = (high + low)/2;
-    var windchill = 35.74 + 0.6215 * t - 35.75 * Math.pow(s, .016) + 0.4275 * t * Math.pow(s, .016);
-    return windchill;
+  document.getElementById('Stown-rainfall').innerHTML =  townData.towns[3].averageRainfall;
 }
